35ec3c21cbc806238b00b905d0a0947e1ebd0e7e
Jack Shea <jackshea@163.com>
2016-01-05 22:45:12 +0800

    初始化Photon Server

---
+++ TestPhotonServer/TestPhotonServer/PPeer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+
+namespace TestPhotonServer
+{
+    public class PPeer:PeerBase
+    {
+        public PPeer(InitRequest initRequest) : base(initRequest)
+        {
+        }
+
+        public PPeer(IRpcProtocol protocol, IPhotonPeer unmanagedPeer) : base(protocol, unmanagedPeer)
+        {
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            var response = new OperationResponse(operationRequest.OperationCode);
+            this.SendOperationResponse(response, sendParameters);
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ TestPhotonServer/TestPhotonServer/PServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photon.SocketServer;
+
+namespace TestPhotonServer
+{
+    public class PServer : ApplicationBase
+    {
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            return new PPeer(initRequest);
+        }
+
+        protected override void Setup()
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected override void TearDown()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
